5ae6ee03e7bd7706c9c92b46a967c29a0091adcc
Nazanin Sinaei <Nazanin.Sinaei@wdc.com>
2018-03-01 00:37:25 -0800

    Changes to pybank folder

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,56 +1,89 @@
 import os
-csvpath=os.path.join(r'C:\Users\33017\Documents\DABootCamp\Day9\03-Python\Homework\Instructions\PyBank\raw_data','budget_data_1.csv')
+from dateutil.parser import parse
+csvpath = os.path.join(r'C:\Users\33017\Desktop\LearnPython\03-Python\Homework\Instructions\PyBank\raw_data\budget_data_2.csv')
 import csv
+
+month_dic = {    1: 'Jan',
+                 2: 'Feb',
+                 3: 'Mar',
+                 4: 'Apr',
+                 5: 'May',
+                 6: 'Jun',
+                 7: 'Jul',
+                 8: 'Aug',
+                 9: 'Sep',
+                 10: 'Oct',
+                 11: 'Nov',
+                 12: 'Dec'
+                 }
 
 with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     #Skip the header of CSV file
     next(csvreader)
-    revenue_dic = {}    
-    budget_data_dic = {}
-    for row in csvreader:
-        budget_data_dic[row[0]] = int(row[1])
-        month, day = row[0].split('-')
-        if month not in revenue_dic.keys():
-            revenue_dic[month] = int(row[1])
-        else:
-            revenue_dic[month] = revenue_dic[month] + int(row[1])
-
-
-revenues = []
-for value in revenue_dic.values():
-    revenues.append(value)
-
-
-revenue_change = []
-for i in range(len(revenues)):
-    if i < len(revenues) - 1:
-         revenue_change.append(revenues[i+1] - revenues[i])
-average_change = sum(revenue_change)/(len(revenues)-1) 
-
-max_revenue = max(budget_data_dic.values())
-min_revenue = min(budget_data_dic.values())
-for key, revenue in budget_data_dic.items():
-    if revenue == max_revenue:
-        max_date = key
-    if revenue == min_revenue:
-        min_date = key
     
-fid = open('results.txt', 'w')    
+    date_revenue_dic = {}
+    
+    for row in csvreader:
+        date = parse(row[0])
+        yy = date.year
+        mm = date.month
+                
+        if (yy, mm) not in date_revenue_dic:
+            date_revenue_dic[yy, mm] = int(row[1])
+        else:
+            date_revenue_dic[yy, mm] = date_revenue_dic[yy, mm] + int(row[1])
+    
+    revenues = []
+    for value in date_revenue_dic.values():
+        revenues.append(value)
+
+
+    revenue_change = []
+    for i in range(len(revenues)-1):
+             revenue_change.append(revenues[i+1] - revenues[i])
+ 
+    diff_revenue_dic = {}    
+    
+    for index, (key, value) in enumerate(date_revenue_dic.items()):
+        if index > 0:
+            diff_revenue_dic[key] = revenue_change[index - 1]
+    
+
+    greatest_increase  = min(diff_revenue_dic.values())
+    greatest_decrease = max(diff_revenue_dic.values())
+    for key, value in diff_revenue_dic.items():
+        
+        if value > greatest_increase:
+            greatest_increase = value
+            greatest_increase_date = key
+            greatest_increase_date_formated = month_dic[greatest_increase_date[1]] + '-' + str(greatest_increase_date[0])
+                
+        if value < greatest_decrease:
+            greatest_decrease = value
+            greatest_decrease_date = key
+            greatest_decrease_date_formated = month_dic[greatest_decrease_date[1]] + '-' + str(greatest_decrease_date[0])
+        
+
+
+
+    
+fid = open('results_budget_data1.txt', 'w')    
 fid.write("Financial Analysis\n")
 fid.write("------------------------------------------------------\n")
-fid.write("Total Months: " + str(len(revenue_dic.keys())) + '\n')
-fid.write("Total Revenue: $" + str(sum(revenue_dic.values())) + '\n')
-fid.write("Greatest increase in revenue: " + max_date + ' ($' + str(max_revenue) + ')' + '\n')
-fid.write("Greatest decrease in revenue: " + min_date + ' ($' + str(min_revenue) + ')' + '\n')
+fid.write("Total Months: " + str(len(date_revenue_dic.keys())) + '\n')
+fid.write("Total Revenue: $" + str(sum(date_revenue_dic.values())) + '\n')
+fid.write("Average Change: $" + str(round(sum(revenue_change)/len(revenue_change), 2)) + '\n')
+fid.write("Greatest increase in revenue: " + greatest_increase_date_formated + ' ($' + str(greatest_increase) + ')' + '\n')
+fid.write("Greatest decrease in revenue: " + greatest_decrease_date_formated + ' ($' + str(greatest_decrease) + ')' + '\n')
 fid.close()
           
 print ("Financial Analysis")
 print("------------------------------------------------------")
-print("Total Months: " + str(len(revenue_dic.keys())))
-print("Total Revenue: $" + str(sum(revenue_dic.values())))
-print("Greatest increase in revenue: " + max_date + ' ($' + str(max_revenue) + ')')
-print("Greatest decrease in revenue: " + min_date + ' ($' + str(min_revenue) + ')')
-
+print("Total Months: " + str(len(date_revenue_dic.keys())))
+print("Total Revenue: $" + str(sum(date_revenue_dic.values())))
+print("Average Change: $" + str(round(sum(revenue_change)/len(revenue_change), 2)))
+print("Greatest increase in revenue: " + greatest_increase_date_formated + ' ($' + str(greatest_increase) + ')')
+print("Greatest decrease in revenue: " + greatest_decrease_date_formated + ' ($' + str(greatest_decrease) + ')')
 
 
